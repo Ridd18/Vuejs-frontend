b02ef35f38952b421d0fdd9b5c1f3c4536619a10
Riddhesh Naik <riddheshnaik786@gmail.com>
2023-03-07 21:22:19 +0530

    download file started

--- src/views/ViewFiles.vue
+++ src/views/ViewFiles.vue
@@ -11,7 +11,9 @@
         <tr v-for="item in items" :key="item.file_id">
           <td>{{ item.name }}</td>
           <td>
-            <button class="btn"><i class="fa fa-download"></i> Download</button>
+            <button class="btn" @click="onClick(item.name)">
+              <i class="fa fa-download"></i> Download
+            </button>
           </td>
           <!-- <div class="card">
             <img src="jeans3.jpg" alt="Denim Jeans" style="width: 100%" />
@@ -51,12 +53,23 @@ export default {
   },
 
   methods: {
-    // Get All Products
+    // Get All files
     async getFiles() {
       try {
         const response = await axios.get("http://localhost:5000/files");
         console.log(response.data);
         this.items = response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+    //download files
+    async onClick(name) {
+      try {
+        const response = await axios.get(`http://localhost:5000/files/${name}`);
+        console.log(response.data);
+        console.log(name);
       } catch (err) {
         console.log(err);
       }
